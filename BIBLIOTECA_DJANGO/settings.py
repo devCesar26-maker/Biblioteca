306a7c02d6bf62e20cb45839fab27a990a80eb45
CesarDjango <dev.cesar26@gmail.com>
2026-05-16 01:49:40 -0300

    Alterações

--- BIBLIOTECA_DJANGO/settings.py
+++ BIBLIOTECA_DJANGO/settings.py
@@ -38,7 +38,8 @@ SECRET_KEY=CHAVE_SECRETA
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Cesar123.pythonanywhere.com']
+
 
 
 # Application definition
@@ -190,6 +191,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
