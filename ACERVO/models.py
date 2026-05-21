692c7ea31c1044452b188668e95c1cd62f2f2255
CesarDjango <dev.cesar26@gmail.com>
2026-05-21 12:04:55 -0300

    migration: remove restricao unique do nome do aluno

--- ACERVO/models.py
+++ ACERVO/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Aluno(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    nome = models.CharField(unique=True, max_length=50)
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
