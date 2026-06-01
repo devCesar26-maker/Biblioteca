b04a14f1ca62f1690adbc88c20f6f6a87cda2815
devCesar26-maker <dev.cesar26@gmail.com>
2026-06-01 10:51:34 -0300

    fix: corrige variável de ambiente

--- BIBLIOTECA_DJANGO/settings.py
+++ BIBLIOTECA_DJANGO/settings.py
@@ -199,12 +199,11 @@ BOOTSTRAP5 = {
 }
 
 
-APP_PASSWORD=os.getenv('APP_PASSWORD')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER="bibliotecadjango105@gmail.com"
-EMAIL_HOST_PASSWORD=os.getenv('APP_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ['APP_PASSWORD']
 DEFAULT_FROM_EMAIL = "Sistema Biblioteca <bibliotecadjango105@gmail.com>"
 
